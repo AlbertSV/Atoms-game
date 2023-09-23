61ba8d4884a4714f5767869eb4b59483fdedfa59
AlbertSV <albert.safin1166@gmail.com>
2023-09-23 13:54:59 +0200

    smth

--- Mendeleev/Dva/Assets/Scripts/Enum.cs
+++ Mendeleev/Dva/Assets/Scripts/Enum.cs
@@ -32,4 +32,18 @@ namespace Dva
         FieldRise,
         FiledShrink
     }
+
+    public enum Materials
+    {
+        Actinides = 1,
+        AlkaliMetal = 2,
+        AlkalineEarthMetal = 3,
+        Lantanides = 4,
+        Metalloids = 5,
+        NobleGas = 6,
+        PostTransitionMetals = 7,
+        ReactiveNonmetals = 8,
+        TransitionMetals = 9,
+        UnknownProperties = 10
+    }
 }
--- Mendeleev/Dva/Assets/Scripts/Progress.cs
+++ Mendeleev/Dva/Assets/Scripts/Progress.cs
@@ -12,11 +12,20 @@ namespace Dva
         {
             _openElemenets = new List<int>();
         }
+
         public void CheckAtomID(int atomID)
         {
             if(AIUtility.GetAtomName.ContainsKey(atomID))
             {
                 int number = (((atomID - 1000000000) % 1000000) % 1000);
+                int nAmount = (atomID - 1000000000) / 1000000;
+
+                if (!_openElemenets.Contains(number))
+                {
+                    _openElemenets.Add(number);
+                }
+
+
             }
         }
     }
